3ee002945c149d2e24c4e3a385d14b02a30a9afa
Anatoliy Ark <anatoliy.arkove@gmail.com>
2017-06-28 22:42:42 +0300

    webpack

--- components/App.js
+++ components/App.js
@@ -1,1 +1,18 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        return
+        (
+            <div> This is definitely a React component. </div>
+        );
+    }
+}
+
+export default App;
+
+
+ //  "tmp": "webpack --config webpack.config.js && nodemon server/server.js"
+
 
