3f612e7ff36131de1f938c7303fbb1a851e43384
Anatoliy Ark <anatoliy.arkove@gmail.com>
2017-06-28 22:46:19 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 var webpack = require('webpack');
 
-module.exports =
-{
-    entry:['./client/client.js'],
-    output:    {
-        path:'./dist',
-        filename:'bundle.js',
-        publicPath:'/'
-    },
-    module: {
-        rules: [{
-            test:/\.js$/,
-            loader:'babel-loader',
-            exclude:/node_modules/,
-            query:{
-               presets:['react','es2015'] 
-            }
-        }]
-    }
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: ['./client/client.js'],
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
 };
