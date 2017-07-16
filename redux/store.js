eb9993c4561f376dd6f65ea712749d64076cf036
Mozart100 <anatoliy.arkove@gmail.com>
2017-07-16 16:42:17 +0300

    adding Redux tools

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import rootReducer from './reducers/index';
+import  {composeWithDevTools} from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 let finalCreateStore =
-    compose(applyMiddleware(thunk,logger))(createStore);
+    //  compose(applyMiddleware(thunk,logger))(createStore);
+     composeWithDevTools(applyMiddleware(thunk,logger))(createStore);
 
 export default function configureStore(
     initialState = { todos: [], user: {} }) {
